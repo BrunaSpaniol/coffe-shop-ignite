9dd144f860db437ea46ba8769890e295eaa139eb
Bruna Spaniol <bruna.spaniol@gmail.com>
2023-09-29 13:28:04 -0300

    configuração das rotas e páginas do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "./Router";
+
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
-      <h1>Hello World</h1>
+      <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
